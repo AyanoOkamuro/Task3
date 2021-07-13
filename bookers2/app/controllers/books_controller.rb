d3df7b36818ae12ffa80247bb49b270d74e402b4
AyanoOkamuro <omochi185@gmail.com>
2021-07-13 07:08:22 +0000

    [update_L15]4-10

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  
+  def index
+  end
+  
+  def show
+  end
+
+  def new #userページにも常に表示されている。ネストする？(親子関係)
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end 
+  
+end
