7dc341d0c936eaa8c606cddbe4186c3e612f385f
AyanoOkamuro <omochi185@gmail.com>
2021-07-15 10:46:50 +0000

    [update_L30-1]4-10

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,34 +1,37 @@
 class BooksController < ApplicationController
-  
+
   def index
-    @user = User.find(current_user.id)
+    #@user = User.find(params[:id])
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     book = Book.new(book_params)
-    book.user_id == current_user
+    book.user_id = current_user.id
     book.save
-    redirect_to book_path(current_user.id)
+    redirect_to book_path(book.id)
   end
-  
+
   def show
+    @user = User.find(params[:id])
+    @new_book = Book.new
+    @book = Book.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
 
   def destroy
-  end 
-  
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
   end
-  
+
 end
