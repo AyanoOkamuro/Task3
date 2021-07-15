67a32ebac44dac7868240aa3f393732a038ee723
AyanoOkamuro <omochi185@gmail.com>
2021-07-15 14:08:45 +0000

    [update_L32]4-10

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -5,13 +5,29 @@ class UsersController < ApplicationController
     @book = Book.new
   end
   
-  def show 
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
   
   def edit
   end
   
   def update
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
   
 end
