4185fe8d9da24208bb5e5bf807c5dfc68a666bca
AyanoOkamuro <omochi185@gmail.com>
2021-07-16 06:10:27 +0000

    [update_L37]4-10

--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '88da81f9a8b912853f81fb4d6a2b4be9781ef141b629c9f5a276d0ffe4d6758b0c76ff718e1b2058e12ab9d227aaf7c469d5aef115a162c8e3fe85593b9113b1'
