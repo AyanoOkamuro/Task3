9c35b4aac60f2069e711dfffecad8e16bcef7426
AyanoOkamuro <omochi185@gmail.com>
2021-07-17 10:16:52 +0000

    [finish]4-10

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -20,11 +20,6 @@ ActiveRecord::Schema.define(version: 2021_07_13_133344) do
     t.integer "user_id"
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,10 +27,10 @@ ActiveRecord::Schema.define(version: 2021_07_13_133344) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "profile_image_id"
+    t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "introduction"
-    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
